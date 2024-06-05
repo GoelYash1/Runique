866d2162fb2b213ee3a1a355db86773259864970
Yash Goel <89704419+GoelYash1@users.noreply.github.com>
2024-06-05 15:46:19 +0530

    Implementation of JvmKtorConventionPlugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.room) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.android.dynamic.feature) apply false
     alias(libs.plugins.mapsplatform.secrets.plugin) apply false
 }
